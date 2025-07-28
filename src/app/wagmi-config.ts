0ffcbde70492ea4aa081818bb5b912a8e491fbc7
abhishek-mvs <saiabhishek546@gmail.com>
2025-07-28 14:33:31 +0530

    Updated the morph network

--- src/app/wagmi-config.ts
+++ src/app/wagmi-config.ts
@@ -1,5 +1,5 @@
 import { http, createConfig } from 'wagmi';
-import { base, baseSepolia, hardhat } from 'wagmi/chains';
+import { base, baseSepolia, hardhat, morphHolesky } from 'wagmi/chains';
 import { injected } from 'wagmi/connectors';
 import { connectorsForWallets, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { metaMaskWallet } from '@rainbow-me/rainbowkit/wallets'; 
@@ -10,9 +10,9 @@ const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '';
 export const config = getDefaultConfig({
   appName: 'chuno',
   projectId: projectId,
-  chains: [baseSepolia],
+  chains: [morphHolesky],
   transports: {
-    [baseSepolia.id]: http('https://84532.rpc.thirdweb.com/2441cd0fdc137b6c6a373a58de2ee453'),
+    [morphHolesky.id]: http(),
   },
   ssr: false, // Disable SSR to prevent hydration issues
 });
